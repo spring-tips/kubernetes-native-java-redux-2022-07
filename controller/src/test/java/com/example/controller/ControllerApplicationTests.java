0f04b43296614c95fd7c967ed3bbc601c6af35a4
Josh Long <josh@joshlong.com>
2022-07-15 14:49:03 -0700

    controller

---
+++ controller/src/test/java/com/example/controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
